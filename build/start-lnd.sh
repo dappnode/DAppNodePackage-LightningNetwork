8a1fd95c0b1ab18986a4321091e820dff938e1d1
vdo <vdo@greyfaze.net>
2018-09-21 12:27:56 +0200

    first commit

---
+++ build/start-lnd.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+# exit from script if error was raised.
+set -e
+
+# error function is used within a bash function in order to send the error
+# message directly to the stderr output and exit.
+error() {
+    echo "$1" > /dev/stderr
+    exit 0
+}
+
+# return is used within bash function in order to return the value.
+return() {
+    echo "$1"
+}
+
+# set_default function gives the ability to move the setting of default
+# env variable from docker file to the script thereby giving the ability to the
+# user override it durin container start.
+set_default() {
+    # docker initialized env variables with blank string and we can't just
+    # use -z flag as usually.
+    BLANK_STRING='""'
+
+    VARIABLE="$1"
+    DEFAULT="$2"
+
+    if [[ -z "$VARIABLE" || "$VARIABLE" == "$BLANK_STRING" ]]; then
+
+        if [ -z "$DEFAULT" ]; then
+            error "You should specify default variable"
+        else
+            VARIABLE="$DEFAULT"
+        fi
+    fi
+
+   return "$VARIABLE"
+}
+
+# Set default variables if needed.
+BITCOIND_HOST=$(set_default "$BITCOIND_HOST" "127.0.0.1")
+RPCUSER=$(set_default "$RPCUSER" "devuser")
+RPCPASS=$(set_default "$RPCPASS" "devpass")
+DEBUG=$(set_default "$DEBUG" "debug")
+NETWORK=$(set_default "$NETWORK" "testnet")
+CHAIN=$(set_default "$CHAIN" "bitcoin")
+BACKEND="bitcoind"
+
+# Use service to guess external IP if not provided (?)
+# curl ident.me
+
+exec lnd \
+    --noseedbackup \
+    --logdir="/data" \
+    "--$CHAIN.active" \
+    "--$CHAIN.$NETWORK" \
+    "--$CHAIN.node"="bitcoind" \
+    "--$BACKEND.rpchost"="$BITCOIND_HOST" \
+    "--$BACKEND.rpcuser"="$RPCUSER" \
+    "--$BACKEND.rpcpass"="$RPCPASS" \
+    "--$BACKEND.zmqpubrawblock=tcp://$BITCOIND_HOST:28332" \
+    "--bitcoind.zmqpubrawtx=tcp://$BITCOIND_HOST:28333" \
+    "--externalip=$EXT_IP" \
+    --debuglevel="$DEBUG" \
+    "$@"
