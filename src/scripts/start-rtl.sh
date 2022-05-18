ff66bf0bc47cbeca3dd5a4c2f1661253a3cf7d97
dapplion <35266934+dapplion@users.noreply.github.com>
2022-05-19 01:25:55 +0700

    Remove unwanted changes

--- src/scripts/start-rtl.sh
+++ src/scripts/start-rtl.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+# Create restore symlink
+if [ ! -L /rtl/backup/restore ]; then
+  ln -s /rtl/backup /rtl/backup/restore
+fi
+
 # Configure settings from env vars
+envsubst < "source_RTL-Config.json" > "RTL-Config.json"
+
 envsubst < "source_RTL.conf" > "RTL.conf"
 
-node rtl
+exec node rtl
